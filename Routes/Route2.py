a9838ae917f6bb89b1f93aa5f2032a952503c364
unknown <marconina999@gmail.com>
2021-04-08 11:28:11 -0400

    Send variables to View

--- Routes/Route2.py
+++ Routes/Route2.py
@@ -6,6 +6,9 @@ app = Flask(__name__, template_folder="View")
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/parameter/<name>')
+def parameter(name):
+    return render_template('index.html', _name = name)
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5001)
+    app.run(debug = True, port = 1999)
